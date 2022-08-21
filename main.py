454e4efbc5b2830313d6d06face5dc1303248f14
Vanshaj Vohra <vanshajvohra@gmail.com>
2022-08-21 12:09:33 +0530

    basic web form

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from crypt import methods
+from unicodedata import name
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -16,3 +21,15 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
+# Form Class
+class firstname(FlaskForm):
+    name = StringField("Enter first name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Create name page
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    name = None
+    form = firstname
+    return render_template("firstname.html")
